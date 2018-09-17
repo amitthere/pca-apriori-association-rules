2202a9db8d2ae0d8996dc88e283aaa8124f786dc
Amit There <amitthere@users.noreply.github.com>
2018-09-16 23:12:22 -0400

    Added data loading class to frequent itemsets

--- association-analysis/frequent_itemsets.py
+++ association-analysis/frequent_itemsets.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 
 class FrequentItemsets:
@@ -7,12 +7,20 @@ class FrequentItemsets:
     itemsets satisfying those criterion
     """
 
-    def __init__(self):
-        pass
-
     def __init__(self, dataset, support):
         self.data = dataset
         self.support = support
 
 
+class ImportData:
+    """
+    Imports data from various sources
+    """
+
+    def __init__(self, file, type):
+        if type == "tab-delimited":
+            self.import_tab_file(file)
+
+    def import_tab_file(self, file):
+        self.data = np.genfromtxt(file, dtype=str, delimiter='\t')
 
