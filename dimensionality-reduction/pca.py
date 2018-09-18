8cb48abe0400102e17c7a3f5a17c87ad92c3c520
sandeshvp <sandeshv@buffalo.edu>
2018-09-18 18:21:09 -0400

    Added code for normalization

--- dimensionality-reduction/pca.py
+++ dimensionality-reduction/pca.py
@@ -8,9 +8,6 @@ class Import:
 
     def __init__(self, file, ftype):
         self.data = None
-        self.data_modified = None
-        self.data_slow = None
-        self.prefixed_data = None
         self.file = file
         if ftype == "TAB":
             self.import_tab_file(self.file)
@@ -18,3 +15,17 @@ class Import:
     def import_tab_file(self, tabfile):
         self.data = np.genfromtxt(tabfile, dtype=str, delimiter='\t')
 
+file1 = Import("../data/pca_a.txt", "TAB")
+file2 = Import("../data/pca_b.txt", "TAB")
+file3 = Import("../data/pca_c.txt", "TAB")
+
+def normalize_matrix(self, file):
+    matrix1 = file1.data[:,0:4]
+    m1 = np.array(matrix1.astype(np.float))
+    #print(m1)
+
+    mean_vector1 = np.mean(m1, axis= 0)
+    #print(mean_vector1)
+
+    centered1 = mean_vector1 - m1
+    print(centered1)
