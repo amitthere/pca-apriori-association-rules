bb5e3d5496f23679dfe7a45e843745a86e79c867
sandeshvp <sandeshv@buffalo.edu>
2018-09-18 15:32:34 -0400

    Initial commit. Added import class.

---
+++ dimensionality-reduction/pca.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+class Import:
+    """
+    Imports data from various sources.
+    Currently, just tab-delimited files are supported
+    """
+
+    def __init__(self, file, ftype):
+        self.data = None
+        self.data_modified = None
+        self.data_slow = None
+        self.prefixed_data = None
+        self.file = file
+        if ftype == "TAB":
+            self.import_tab_file(self.file)
+
+    def import_tab_file(self, tabfile):
+        self.data = np.genfromtxt(tabfile, dtype=str, delimiter='\t')
+
