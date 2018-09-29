2b438fe155702eb3def3c36a84cb1b18be351ed5
Amit There <amitthere@users.noreply.github.com>
2018-09-29 16:11:34 -0400

    Skeleton for association rule templates

--- association-analysis/association_rules.py
+++ association-analysis/association_rules.py
@@ -1,5 +1,22 @@
 import numpy as np
 
+
+class AssociationRules:
+
+    def __init__(self, confidence):
+        self.confidence = confidence
+
+    def template1(self, arg1, arg2, arg3):
+        return
+
+    def template2(self, arg1, arg2):
+        return
+
+    def template3(self, type, *args):
+        return
+
+
+
 def main():
     return
 
